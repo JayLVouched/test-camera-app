df5a97c32c643cc17a681aed395f679211e4ada8
Jay Lorenzo <jay.lorenzo@vouched.id>
2022-04-12 09:49:52 -0700

    Simple camera app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+
+import React, { useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+
+  const videoRef = useRef(null);
+
+  async function loadShims()  {
+
+  } 
+
+  //var constraints = { video: { width: 400, height: 400, facingMode: { exact: "user" } } };
+  // var constraints = { video: { width: 400, height: 400 }};
+  // let promise = navigator.mediaDevices.getUserMedia(constraints);
+
+  // promise.then((stream) => {
+  //   console.log('stream is active ' + stream.active);
+  //   const constraints = navigator.mediaDevices.getSupportedConstraints();
+  //   constraints.forEach( (constraint) =>{
+  //     console.log(constraint); 
+  //   });
+
+  //   navigator.mediaDevices.enumerateDevices()
+  //     .then( (mediaDevices) => {
+  //       mediaDevices.forEach( (mediaDevice) =>{
+  //         console.log(JSON.stringify(mediaDevice.toJSON())); 
+  //       });
+  //     });
+  // });
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
+  const getVideo = () => {
+
+    const mediaConstraints = { video: { width: 400, height: 400, facingMode: "user" } };
+
+    navigator.mediaDevices
+      .getUserMedia(mediaConstraints)
+      .then(stream => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(err => {
+        console.error("error:", err);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <video ref={videoRef}/>
+    <canvas />
+  </div>
   );
 }
 
